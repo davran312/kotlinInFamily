a3b1d05fec479b6edfb798fbd8aed8a870d32ce1
entezeer <ntz.talgat98@gmail.com>
2018-07-27 23:17:56 +0600

    add UI tests

--- app/src/androidTest/java/com/example/virus/kotlininfamily/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/virus/kotlininfamily/ExampleInstrumentedTest.kt
@@ -1,12 +1,25 @@
 package com.example.virus.kotlininfamily
 
+import android.content.pm.ActivityInfo
 import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.runner.AndroidJUnit4
-
+import com.example.virus.kotlininfamily.ui.main.MainMenuActivity
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.action.ViewActions.*
 import org.junit.Test
 import org.junit.runner.RunWith
-
+import android.support.test.rule.ActivityTestRule
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main_menu.*
 import org.junit.Assert.*
+import org.junit.Rule
+import java.util.*
+import android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition
+import android.support.test.espresso.matcher.ViewMatchers.isRoot
+import com.example.virus.kotlininfamily.R.id.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +27,62 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class UiTest {
+    @get:Rule
+    val mainRule = ActivityTestRule(
+            MainMenuActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.virus.kotlininfamily", appContext.packageName)
+    fun orientationTest(){
+        onView(withId(R.id.recyclerView)).perform(swipeRight())
+        mainRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+        mainRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+    }
+    @Test
+    fun clickTest(){
+      onView(withId(recyclerView)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        Thread.sleep(2000)
+        onView(isRoot()).perform(pressBack())
+        onView(withId(recyclerView)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click()))
+        for (i in 0..2){
+            onView(withId(recyclerView)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(i, click()))
+            Thread.sleep(1000)
+            onView(isRoot()).perform(pressBack())
+        }
+      onView(withId(recyclerView)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
+        for (i in 0..2){
+            onView(withId(recyclerView)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(i, click()))
+            Thread.sleep(1000)
+            onView(isRoot()).perform(pressBack())
+        }
+
+
+        onView(withId(recyclerView)).perform(swipeUp())
+    }
+    @Test
+fun becomeParentActivityTest(){
+        onView(withId(recyclerView)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        Thread.sleep(2000)
+        onView(withId(information)).perform(click())
+        onView(withId(mViewPager)).perform(swipeLeft())
+        onView(withId(mViewPager)).perform(swipeUp())
+        onView(withId(mViewPager)).perform(swipeDown())
+        Thread.sleep(1000)
+        onView(withId(mViewPager)).perform(swipeRight())
+        onView(withId(mViewPager)).perform(swipeUp())
+        onView(withId(mViewPager)).perform(swipeDown())
+        Thread.sleep(1000)
+        onView(withId(mViewPager)).perform(swipeLeft())
+        onView(withId(mViewPager)).perform(swipeUp())
+        onView(withId(mViewPager)).perform(swipeDown())
+        Thread.sleep(1000)
+        onView(withId(mViewPager)).perform(swipeLeft())
+        onView(withId(mViewPager)).perform(swipeUp())
+        onView(withId(mViewPager)).perform(swipeDown())
+        Thread.sleep(1000)
+        onView(withId(mViewPager)).perform(swipeRight())
+        onView(withId(mViewPager)).perform(swipeUp())
+        onView(withId(mViewPager)).perform(swipeDown())
+        onView(isRoot()).perform(pressBack())
     }
 }
---
+++ app/src/androidTest/java/com/example/virus/kotlininfamily/TestTesting.kt
@@ -0,0 +1,43 @@
+package com.example.virus.kotlininfamily
+
+import android.content.pm.ActivityInfo
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.runner.AndroidJUnit4
+import com.example.virus.kotlininfamily.ui.main.MainMenuActivity
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.action.ViewActions.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import android.support.test.rule.ActivityTestRule
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main_menu.*
+import org.junit.Assert.*
+import org.junit.Rule
+import java.util.*
+import android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.isRoot
+import com.example.virus.kotlininfamily.R.id.*
+import com.example.virus.kotlininfamily.ui.main.section_become_parent.section_test.TestActivity
+
+@RunWith(AndroidJUnit4::class)
+class TestTesting {
+    @get:Rule
+    val mainRule = ActivityTestRule(
+            TestActivity::class.java)
+    @Test
+    fun runTest(){
+        onView(withId(R.id.btn_result)).perform(ViewActions.click())
+        Thread.sleep(2000)
+        for (i in 0..14){
+        onView(withId(recyclerView)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(i, click()))
+       }
+        onView(withId(R.id.btn_result)).perform(ViewActions.click())
+
+
+    }
+}
